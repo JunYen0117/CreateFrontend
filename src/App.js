4177acf85f18113bd96f7ec528c23fc99fe61afc
Miu37 <ispan8937@gmail.com>
2022-06-13 23:12:57 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ProductOrder from './pages/ProductOrder';
 
 function App() {
   return (
     <>
       <Header />
+      <ProductOrder />
       <Footer />
     </>
   );
