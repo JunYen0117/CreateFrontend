d17d47a0478018a98b2466c833260c26c9a2afb9
Miu37 <ispan8937@gmail.com>
2022-06-15 09:19:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Product from './pages/Product';
+import About from './pages/About';
+import User from './pages/User';
+import Home from './pages/Home';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
+      <Switch>
+        <Route path="/product">
+          <Product />
+        </Route>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
