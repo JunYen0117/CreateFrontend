c73c37d9a1f9c500bcc924789a5b416b2a6450c9
Miu37 <ispan8937@gmail.com>
2022-06-16 23:29:51 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { useState } from 'react';
 
 import Product from './pages/Product';
 import About from './pages/About';
 import User from './pages/User';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
-import ProductOrder from './pages/ProductOrder';
+import OrderList from './pages/ProductOrder/OrderList';
+import OrderDetail from './pages/ProductOrder/OrderDetail';
 
 function App() {
+  const [isShowOL, setIsShowOL] = useState(true);
+  const [isShowOD, setIsShowOD] = useState(false);
   return (
     <>
       <Header />
-      <ProductOrder />
+      {isShowOL ? (
+        <OrderList showOL={setIsShowOL} showOD={setIsShowOD} />
+      ) : null}
+      {isShowOD ? <OrderDetail /> : null}
       <Router>
         <Switch>
           <Route path="/product">
