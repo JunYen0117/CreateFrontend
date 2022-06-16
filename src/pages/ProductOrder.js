20d7dc6cec0d51d0aafc24991dd8e59741b9334f
Miu37 <ispan8937@gmail.com>
2022-06-16 10:17:36 +0800

    Update ProductOrder.js

--- src/pages/ProductOrder.js
+++ src/pages/ProductOrder.js
@@ -1,16 +1,4 @@
-const ProductOrder = () => {
-  return (
-    <>
-      {/* 沒有訂單 */}
-      {/* <h1>我的訂單</h1>
-      <div className="border-bottom">
-        <a href="#">已完成</a>
-        <a href="#">已取消</a>
-      </div>
-
-      <div>
 import Sidebar from '../components/Sidebar';
-import { ReactComponent as More } from '../img/More.svg';
 
 const ProductOrder = () => {
   const arr = [];
@@ -123,34 +111,6 @@ const ProductOrder = () => {
         <h3>您沒有尚未付款的訂單</h3>
       </div> */}
 
-      {/* ===================================== */}
-
-      {/* 已完成 */}
-
-      <h1>我的訂單</h1>
-      <div className="border-bottom">
-        <a href="#/">已完成</a>
-        <a href="#/">已取消</a>
-      </div>
-      <div className="orderlist-card">
-        <div className="card-title d-flex">
-          <p className="text-info">5555</p>
-          <div className="title-num">20221212</div>
-          <div className="title-name">Miu</div>
-        </div>
-        <div className="card-content">
-          <table>
-            <tr>
-              <td className="text-info">State</td>
-              <td>finish</td>
-            </tr>
-            <tr>
-              <td>account</td>
-              <td>14</td>
-            </tr>
-          </table>
-        </div>
-        <button className="card-button">查看訂單明細</button>
             {/* ===================================== */}
 
             {/* 訂單明細 */}
