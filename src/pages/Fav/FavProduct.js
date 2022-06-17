c64010e5a57b08878b3b9cdb4967eb4f54fae9cb
Miu37 <ispan8937@gmail.com>
2022-06-17 14:17:55 +0800

    Update FavProduct.js

--- src/pages/Fav/FavProduct.js
+++ src/pages/Fav/FavProduct.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import ASidebar from '../../components/AccountSidebar/ASidebar';
+import FavTitle from '../../components/Fav/FavTitle';
 
 const FavProduct = () => {
   return (
-    <div>FavProduct</div>
-  )
-}
+    <>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <ASidebar />
+          <div className="col-9 productOrder">
+            <FavTitle />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default FavProduct
+export default FavProduct;
