66fbdedc1c79bb7af32026f9031c8712ad821f43
Miu37 <ispan8937@gmail.com>
2022-06-17 16:50:23 +0800

    Update FavProduct.js

--- src/pages/Fav/FavProduct.js
+++ src/pages/Fav/FavProduct.js
@@ -18,19 +18,21 @@ const FavProduct = () => {
                 return (
                   <Card
                     style={{ width: '13rem' }}
-                    className="me-5 p-0 position-relative"
+                    className="me-5 mt-2 p-0 position-relative"
                   >
                     <Card.Img variant="top" src={Bird} className="Fp_img" />
                     <FaHeart className="Fp_heart" />
                     <Card.Body>
-                      <Card.Title className="Fp_cardtitle">鳥燈</Card.Title>
-                      <Card.Text>NT$ 260</Card.Text>
+                      <Card.Title className="Fp_cardtitle text-center">
+                        鳥燈
+                      </Card.Title>
+                      <Card.Text className="text-center">NT$ 260</Card.Text>
                       <Button
                         style={{ width: '10rem' }}
                         variant="primary"
-                        className="text-white"
+                        className="text-white mx-auto d-block"
                       >
-                        <FaShoppingCart className="me-3" />
+                        <FaShoppingCart className="me-3 " />
                         加入購物車
                       </Button>
                     </Card.Body>
