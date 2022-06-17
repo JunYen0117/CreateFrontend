6777eca5d2890f87b1ecd84cbccee4da1c6b43fa
Miu37 <ispan8937@gmail.com>
2022-06-17 16:42:26 +0800

    Update FavProduct.js

--- src/pages/Fav/FavProduct.js
+++ src/pages/Fav/FavProduct.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ASidebar from '../../components/AccountSidebar/ASidebar';
 import FavTitle from '../../components/Fav/FavTitle';
+import { Card, Button } from 'react-bootstrap';
+import Bird from '../../img/Bird.jpg';
+import { FaShoppingCart, FaHeart } from 'react-icons/fa';
 
 const FavProduct = () => {
   return (
@@ -10,6 +13,31 @@ const FavProduct = () => {
           <ASidebar />
           <div className="col-9 productOrder">
             <FavTitle />
+            <div className=" row">
+              {[1, 2, 3, 4].map(() => {
+                return (
+                  <Card
+                    style={{ width: '13rem' }}
+                    className="me-5 p-0 position-relative"
+                  >
+                    <Card.Img variant="top" src={Bird} className="Fp_img" />
+                    <FaHeart className="Fp_heart" />
+                    <Card.Body>
+                      <Card.Title className="Fp_cardtitle">鳥燈</Card.Title>
+                      <Card.Text>NT$ 260</Card.Text>
+                      <Button
+                        style={{ width: '10rem' }}
+                        variant="primary"
+                        className="text-white"
+                      >
+                        <FaShoppingCart className="me-3" />
+                        加入購物車
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
