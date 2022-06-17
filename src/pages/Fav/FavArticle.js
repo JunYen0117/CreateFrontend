3ce3d0391ed623eb7d4045e7279f6b6dd83c02c5
Miu37 <ispan8937@gmail.com>
2022-06-17 14:17:53 +0800

    Update FavArticle.js

--- src/pages/Fav/FavArticle.js
+++ src/pages/Fav/FavArticle.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import ASidebar from '../../components/AccountSidebar/ASidebar';
+
 
 const FavArticle = () => {
   return (
-    <div>FavArticle</div>
+    <>
+    <div className="container-fluid mb-5">
+      <div className="row">
+        <ASidebar />
+      </div>
+    </div>
+  </>
   )
 }
 
