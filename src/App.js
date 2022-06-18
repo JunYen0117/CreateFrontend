ae9b2de389f40a57eb7ed1062f035222bbbd431a
Miu37 <ispan8937@gmail.com>
2022-06-18 23:08:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import FavProduct from './pages/Fav/FavProduct';
 import FavActivity from './pages/Fav/FavActivity';
 import FavArticle from './pages/Fav/FavArticle';
 
+
 function App() {
   const [isShowOL, setIsShowOL] = useState(true);
   const [isShowOD, setIsShowOD] = useState(false);
@@ -23,9 +24,9 @@ function App() {
       <Router>
         <Switch>
           <Route path="/fav">
-            <FavProduct />
-            {/* <FavArticle />
-            <FavActivity /> */}
+            {/* <FavProduct /> */}
+            <FavArticle />
+            {/* <FavActivity /> */}
           </Route>
           <Route path="/order">
             {isShowOL ? (
