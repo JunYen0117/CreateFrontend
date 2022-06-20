6b933c2d50b79c3415ffbd14cd84b6dab479d4b5
Miu37 <ispan8937@gmail.com>
2022-06-20 10:58:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Home from './pages/Home';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
   return (
     <>
       <Header />
+      <Sidebar />
       <Router>
         <Switch>
           <Route path="/product">
