3dba47b095a9aae949fa1a8aa4343648a62e79cd
immindy4527 <immindy4527@gmail.com>
2022-06-20 16:32:50 +0800

    輪播按鈕調整

--- src/components/FrontPage/CarouselContents.js
+++ src/components/FrontPage/CarouselContents.js
@@ -5,19 +5,21 @@ function CarouselContents(props) {
     const a = index == 0 ? 'active' : '';
     return (
       <div className={`carousel-item ${a}`}>
-        <div className="d-flex justify-content-between all-logo">
+        <div className="all-logo d-flex justify-content-between">
           {page_content.map((content) => (
-            <Card className="col-3">
-              <a href="/">
-                <figure className="logo">
-                  <Card.Img variant="top" src={content.img} className="img" />
-                </figure>
-              </a>
-              <Card.Body>
-                <Card.Title className="d-flex justify-content-center">
-                  <a href="/">{content.brand_name}</a>
-                </Card.Title>
-              </Card.Body>
+            <Card >
+              <div className="logo-card">
+                <a href="/">
+                  <figure className="logo-pic">
+                    <Card.Img variant="top" src={content.img} className="img" />
+                  </figure>
+                </a>
+                <Card.Body>
+                  <Card.Title className="d-flex justify-content-center">
+                    <a href="/">{content.brand_name}</a>
+                  </Card.Title>
+                </Card.Body>
+              </div>
             </Card>
           ))}
         </div>
