57233a68488ad2f275b8b62ab13eb7a90c57c30d
Miu37 <ispan8937@gmail.com>
2022-06-20 10:38:59 +0800

    Update FavProduct.js

--- src/pages/Fav/FavProduct.js
+++ src/pages/Fav/FavProduct.js
@@ -13,35 +13,26 @@ const FavProduct = () => {
           <ASidebar />
           <div className="col-8 productOrder">
             <FavTitle />
-            <div className=" row">
-              {[1, 2, 3, 4].map(() => {
+            <div className=" row d-flex">
+              {[1, 2, 3, 4, 5].map(() => {
                 return (
-                  <Card
-                    style={{ width: '12rem' }}
-                    className="me-5 mt-2 p-0 position-relative"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src={Bird}
-                      className="Fp_img"
-                      style={{ height: '10rem' }}
-                    />
-                    <FaHeart className="Fp_heart" />
-                    <Card.Body>
-                      <Card.Title className="Fp_cardtitle text-center">
-                        鳥燈
-                      </Card.Title>
-                      <Card.Text className="text-center">NT$ 260</Card.Text>
+                  <div className="col-2">
+                    <div className="  Fp_top ">
+                      <img className="Fp_img" src={Bird} alt="" />
+                    </div>
+                    <div className="Fp_body">
+                      <h2>鳥燈</h2>
+                      <p>NT $100</p>
                       <Button
                         style={{ width: '10rem' }}
                         variant="primary"
-                        className="text-white mx-auto d-block"
+                        className="text-white d-block"
                       >
-                        <FaShoppingCart className="me-3 " />
+                        <FaShoppingCart className="me-3 h5" />
                         加入購物車
                       </Button>
-                    </Card.Body>
-                  </Card>
+                    </div>
+                  </div>
                 );
               })}
             </div>
