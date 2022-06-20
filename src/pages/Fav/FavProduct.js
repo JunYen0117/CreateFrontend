1655456276c0946802576eea5db96772295408e0
Miu37 <ispan8937@gmail.com>
2022-06-20 14:07:38 +0800

    Update FavProduct.js

--- src/pages/Fav/FavProduct.js
+++ src/pages/Fav/FavProduct.js
@@ -8,35 +8,29 @@ import { FaShoppingCart, FaHeart } from 'react-icons/fa';
 const FavProduct = () => {
   return (
     <>
-      <div className="container-fluid mb-5">
-        <div className="row">
-          <ASidebar />
-          <div className="col-8 productOrder">
-            <FavTitle />
-            <div className=" row d-flex">
-              {[1, 2, 3, 4, 5].map(() => {
-                return (
-                  <div className="col-2">
-                    <div className="  Fp_top ">
-                      <img className="Fp_img" src={Bird} alt="" />
-                    </div>
-                    <div className="Fp_body">
-                      <h2>鳥燈</h2>
-                      <p>NT $100</p>
-                      <Button
-                        style={{ width: '10rem' }}
-                        variant="primary"
-                        className="text-white d-block"
-                      >
-                        <FaShoppingCart className="me-3 h5" />
-                        加入購物車
-                      </Button>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+      <div className="container-fluid mb-5 px-0">
+        <div className=" row d-flex">
+          {[1, 2, 3, 4, 5].map(() => {
+            return (
+              <div className="col-2">
+                <div className="  Fp_top ">
+                  <img className="Fp_img" src={Bird} alt="" />
+                </div>
+                <div className="Fp_body">
+                  <h2>鳥燈</h2>
+                  <p>NT $100</p>
+                  <Button
+                    style={{ width: '10rem' }}
+                    variant="primary"
+                    className="text-white d-block"
+                  >
+                    <FaShoppingCart className="me-3 h5" />
+                    加入購物車
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
