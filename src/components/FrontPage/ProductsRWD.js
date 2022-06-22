2f83a9b5b4b0c28b20aa97ff2969f2cecd5d9f0b
CHINF <chinf1996@gmail.com>
2022-06-23 00:53:24 +0800

    feat: 以 `window.matchMedia` 修改原 RWD 的設計
    `ProductRWD` 和 `ProductCard` 雷同，因此刪除。

--- src/components/FrontPage/ProductsRWD.js
+++ src/components/FrontPage/ProductsRWD.js
@@ -1,6 +1,5 @@
 // import '../../styles/_frontpage.scss';
-import ProductRWD from './ProductRWD';
-
+import ProductCard from './ProductCard';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
@@ -10,33 +9,31 @@ function ProductsRWD(props) {
   const { datas } = props;
   return (
     <>
-      {datas.map((Productdatas, index) => {
-        const a = index === 0 ? 'active' : '';
-        return (
-          <Swiper
-            spaceBetween={15}
-            slidesPerView={2}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            <div className="all-logo d-flex justify-content-between">
-              {Productdatas.map((content) => (
-                <SwiperSlide className={`carousel-item ${a}`}>
-                  <ProductRWD
-                    key={content.product}
-                    product={content.product}
-                    brand={content.brand}
-                    product_pic={content.product_pic}
-                    brand_href={content.brand_href}
-                    pic_href={content.pic_href}
-                    cost={content.cost}
-                  />
-                </SwiperSlide>
-              ))}
-            </div>
-          </Swiper>
-        );
-      })}
+      <Swiper
+        spaceBetween={3}
+        slidesPerView={2}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {datas.map((Productdatas, index) => {
+          const a = index === 0 ? 'active' : '';
+          return (
+            Productdatas.map((content) => (
+              <SwiperSlide>
+                <ProductCard
+                  key={content.product}
+                  product={content.product}
+                  brand={content.brand}
+                  product_pic={content.product_pic}
+                  brand_href={content.brand_href}
+                  pic_href={content.pic_href}
+                  cost={content.cost}
+                />
+              </SwiperSlide>
+            ))
+          );
+        })}
+      </Swiper>
     </>
   );
 }
