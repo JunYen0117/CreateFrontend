2096e0448ee7c6571dd780269b5c55ef49ce5dc9
immindy4527 <immindy4527@gmail.com>
2022-06-23 12:04:45 +0800

    card

--- src/components/BrandPage/Brandcard.js
+++ src/components/BrandPage/Brandcard.js
@@ -1,22 +1,58 @@
 import Banner from '../../img/banner.jpg';
-// import Logo from '../../img/purple.jpg';
+import Logo from '../../img/purple.jpg';
 
 function Brandcard(props) {
-  const { vendor } = props;
+  // const { vendor } = props;
   return (
     <>
-      <div className="col-4 brand">
-        <div class="card brand-card">
-          <img src={Banner} className="card-img-top brand-banner" alt="..." />
-          <div class="card-body d-flex justify-content-between">
-            <img
-              src={`http://localhost:3003/images/product/${vendor.logo}`}
-              className="brand-logo"
-              alt=""
-            />
-            <div className="brand-imformation">
-              <h2>{vendor.business_name}</h2>
-              <p class="card-text">{vendor.introduce}</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 brand">
+            <div class="card brand-card">
+              <img
+                src={Banner}
+                className="card-img-top brand-banner"
+                alt="..."
+              />
+              <div class="card-body d-flex justify-content-">
+                <img src={Logo} className="brand-logo" alt="" />
+                <div className="brand-imformation">
+                  <h2>店家名稱</h2>
+                  <p class="card-text">fffffffffffffffffffff</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 brand">
+            <div class="card brand-card">
+              <img
+                src={Banner}
+                className="card-img-top brand-banner"
+                alt="..."
+              />
+              <div class="card-body d-flex justify-content-start">
+                <img src={Logo} className="brand-logo" alt="" />
+                <div className="brand-imformation">
+                  <h2>店家名稱</h2>
+                  <p class="card-text">fffffffffffffffffffff</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 brand">
+            <div class="card brand-card">
+              <img
+                src={Banner}
+                className="card-img-top brand-banner"
+                alt="..."
+              />
+              <div class="card-body d-flex justify-content-start">
+                <img src={Logo} className="brand-logo" alt="" />
+                <div className="brand-imformation">
+                  <h2>店家名稱</h2>
+                  <p class="card-text">fffffffffffffffffffff</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
