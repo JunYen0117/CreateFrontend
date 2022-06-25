4f9bedf8c6041e02edca319dc8f0799bd5a8f1ac
JunYen0117 <relife12345@gmail.com>
2022-06-25 12:16:04 +0800

    edit starter.js, _starter.scss; 點擊動畫效果

--- src/pages/Starter.js
+++ src/pages/Starter.js
@@ -1,24 +1,42 @@
+import { useState } from 'react';
+
 function Starter() {
+  const [cssFlying1, setCssFlying1] = useState('noflying');
+  const [cssFlying2, setCssFlying2] = useState('noflying');
+  const [cssFlying3, setCssFlying3] = useState('noflying');
+  const changeCss1 = () => {
+    setCssFlying2('flying');
+    setCssFlying3('flying');
+  };
+  const changeCss2 = () => {
+    setCssFlying1('flying');
+    setCssFlying3('flying');
+  };
+  const changeCss3 = () => {
+    setCssFlying1('flying');
+    setCssFlying2('flying');
+  };
+
   return (
     <>
       <ul className="starter_flex d-flex flex-column flex-md-row">
-        <li className="starter_flex_item_left">
-          <a className="link" href="#/" alt="#/">
+        <li className={`starter_flex_item_left ${cssFlying1}`}>
+          <a className="link" href="#/" alt="#/" onClick={changeCss1}>
             <span className="text">課程展覽</span>
           </a>
         </li>
-        <li className="starter_flex_item_mid">
-          <a className="link" href="#/" alt="#/">
+        <li className={`starter_flex_item_mid ${cssFlying2}`}>
+          <a className="link" href="#/" alt="#/" onClick={changeCss2}>
             <span className="text">買禮物</span>
           </a>
         </li>
-        <li className="starter_flex_item_right">
-          <a className="link" href="#/" alt="#/">
+        <li className={`starter_flex_item_right ${cssFlying3}`}>
+          <a className="link" href="#/" alt="#/" onClick={changeCss3}>
             <span className="text">藝文誌</span>
           </a>
         </li>
       </ul>
-      <a className="starter_skip p-2" href="aaaa/" alt="#/">
+      <a className="starter_skip p-2" href="#/" alt="#/">
         ＳＫＩＰ
       </a>
     </>
