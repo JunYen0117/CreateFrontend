987434ca20d96748625d0f2b1881ac6f5d06af4b
Miu37 <ispan8937@gmail.com>
2022-06-27 16:10:56 +0800

    Update Order.js

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ASidebar from '../components/AccountSidebar/ASidebar';
 import List from '../components/ProductOrder/List';
-// import ListCancel from '../components/ProductOrder/ListCancel';
+import ListCancel from '../components/ProductOrder/ListCancel';
 import { useState, useEffect } from 'react';
 import { Tab, Col, Nav, Row } from 'react-bootstrap';
 import { API_URL } from '../utils/config';
@@ -9,18 +9,6 @@ import axios from 'axios';
 import 'antd/dist/antd.css';
 
 const Order = () => {
-  // const [isShowOD, setIsShowOD] = useState(false);
-  const [orders, setOrders] = useState([]);
-
-  useEffect(() => {
-    let getOrders = async () => {
-      // axios.get(URL, config)
-      let response = await axios.get(API_URL + '/productorder');
-      setOrders(response.data);
-    };
-    getOrders();
-  }, []);
-  // console.log(orders);
   return (
     <>
       <Tab.Container id="left-tabs-example" defaultActiveKey="finish">
@@ -28,7 +16,17 @@ const Order = () => {
           <ASidebar />
           <Col sm={8}>
             <h1 className="Fl_title  ">我的訂單</h1>
-            <Nav className="Fl_nav" defaultActiveKey="finish">
+            <Nav className="Fl_nav" defaultActiveKey="notshipped">
+              <Nav.Item className=" Fl_item mt-3 ">
+                <Nav.Link className="Fl_btn h3 " eventKey="notshipped">
+                  未出貨
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="  Fl_item mt-3 ">
+                <Nav.Link className="Fl_btn h3" eventKey="shipped">
+                  已出貨
+                </Nav.Link>
+              </Nav.Item>
               <Nav.Item className=" Fl_item mt-3 ">
                 <Nav.Link className="Fl_btn h3" eventKey="finish">
                   已完成
@@ -42,10 +40,18 @@ const Order = () => {
             </Nav>
             <Col sm={12}>
               <Tab.Content className="mb-5 row mt-3">
+                <Tab.Pane eventKey="notshipped">
+                  <List />
+                </Tab.Pane>
+                <Tab.Pane eventKey="shipped">
+                  <List />
+                </Tab.Pane>
                 <Tab.Pane eventKey="finish">
                   <List />
                 </Tab.Pane>
-                <Tab.Pane eventKey="cancel">{/* <ListCancel /> */}</Tab.Pane>
+                <Tab.Pane eventKey="cancel">
+                  <ListCancel />
+                </Tab.Pane>
               </Tab.Content>
             </Col>
           </Col>
