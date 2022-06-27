928529625bf46e3927d62ac0cd1f966d447b779c
cathysyu <siangjyun0926@gmail.com>
2022-06-27 17:44:29 +0800

    update pagination & page

--- src/components/coupon/Common.js
+++ src/components/coupon/Common.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import Card from './Card';
 import Pagination from 'react-bootstrap/Pagination';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../utils/config';
 
 function Common(props) {
   const { prompt, pass, use } = props;
+  const [couponList, setCoupons] = useState([]);
+  const [nowPage, setNowPage] = useState(1);
+  const [lastPage, setLastPage] = useState();
+
+  useEffect(() => {
+    let getCoupons = async () => {
+      let response = await axios.get(API_URL + '/coupons', {
+        params: {
+          page: nowPage,
+        },
+      });
+      setCoupons(response.data.couponList);
+      setLastPage(response.data.pagination.lastPage);
+    };
+    getCoupons();
+  }, [nowPage, lastPage]);
+
   const [active, setActive] = useState(1);
   let items = [];
-  for (let number = 1; number <= 3; number++) {
+  for (let number = 1; number <= lastPage; number++) {
     items.push(
       <Pagination.Item
         key={number}
         active={number === active}
         onClick={(e) => {
           setActive(number);
+          setNowPage(number);
+          setLastPage(number);
         }}
       >
         {number}
@@ -28,8 +49,11 @@ function Common(props) {
           {/* 這是 每筆訂單僅可使用一張優惠券 的變數 */}
           {prompt}
         </h4>
+        {/* div不能放在map裡面，因為這樣 d-flex 會一直重複生成，所以要放在外面 */}
         <div className="coupon d-flex d-grid flex-wrap justify-content-between mx-1 row">
-          <Card pass={pass} use={use} />
+          {couponList.map((coupon, index) => {
+            return <Card pass={pass} use={use} coupon={coupon} />;
+          })}
         </div>
       </div>
       <div>
