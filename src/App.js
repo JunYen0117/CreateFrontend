f06b12e8bee0e99c3c93cc7480cf7d98524a646e
JunYen0117 <relife12345@gmail.com>
2022-06-30 12:16:50 +0800

    edit app.js

--- src/App.js
+++ src/App.js
@@ -34,66 +34,69 @@ import ChangeStartFlag from './components/ChangeStartFlag';
 import FavList from './pages/Fav/FavList';
 import Order from './pages/ProductOrder/Order';
 function App() {
-  const [start, setStart] = useState(true);
+  const [start, setStart] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
   return (
     <CartProvider localStorageKey="Cart">
       <Router>
         <>
+          <Route exact path="/">
+            <Starter />
+          </Route>
           <Header start={start} isLogin={isLogin} setIsLogin={setIsLogin} />
-          <ChangeStartFlag setStart={setStart}>
-            <Switch>
-              <Route path="/Front">
-                <FrontPage />
-              </Route>
-              <Route path="/Brand">
-                <BrandPage />
-              </Route>
-              <Route path="/BrandStores">
-                <BrandStores />
-              </Route>
-              <Route path="/course">
-                <Course />
-              </Route>
-              <Route path="/activity">
-                <Activity />
-              </Route>
-              <Route path="/activitypayment">
-                <ActivityPayment />
-              </Route>
-              <Route path="/member">
-                <Member />
-              </Route>
-              <Route path="/themeplanning">
-                <ThemePlanning />
-              </Route>
-              <Route path="/pwdchanging">
-                <PwdChanging />
-              </Route>
-              <Route path="/product/detail">
-                <ProductDetail />
-              </Route>
-              <Route path="/product">
-                <Products />
-              </Route>
-              <Route path="/cart">
-                <Cart />
-              </Route>
-              <Route path="/fav">
-                <FavList />
-              </Route>
-              <Route path="/order">
-                <Order />
-              </Route>
-              <Route path="/exhibition">
-                <Exhibition />
-              </Route>
-              <Route path="/exhibitionexplore">
-                <ExhibitionExploret />
-              </Route>
-              <Route path="/">{start ? <Starter /> : () => <h1>123</h1>}</Route>
-            </Switch>
-          </ChangeStartFlag>
+          {/* <ChangeStartFlag setStart={setStart}> */}
+          <Switch>
+            <Route path="/Front">
+              <FrontPage />
+            </Route>
+            <Route path="/Brand">
+              <BrandPage />
+            </Route>
+            <Route path="/BrandStores">
+              <BrandStores />
+            </Route>
+            <Route path="/course">
+              <Course />
+            </Route>
+            <Route path="/activity">
+              <Activity />
+            </Route>
+            <Route path="/activitypayment">
+              <ActivityPayment />
+            </Route>
+            <Route path="/member">
+              <Member />
+            </Route>
+            <Route path="/themeplanning">
+              <ThemePlanning />
+            </Route>
+            <Route path="/pwdchanging">
+              <PwdChanging />
+            </Route>
+            <Route path="/product/detail">
+              <ProductDetail />
+            </Route>
+            <Route path="/product">
+              <Products />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/fav">
+              <FavList />
+            </Route>
+            <Route path="/order">
+              <Order />
+            </Route>
+            <Route path="/exhibition">
+              <Exhibition />
+            </Route>
+            <Route path="/exhibitionexplore">
+              <ExhibitionExploret />
+            </Route>
+            {/* <Route path="/">{start ? <Starter /> : () => <h1>123</h1>}</Route> */}
+          </Switch>
+          {/* </ChangeStartFlag> */}
           <Footer start={start} />
         </>
       </Router>
