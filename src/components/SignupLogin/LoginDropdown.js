67f6585da52b99b4b3e72d9fa472d4c77af88e7e
JunYen0117 <relife12345@gmail.com>
2022-06-30 14:51:46 +0800

    edit

--- src/components/SignupLogin/LoginDropdown.js
+++ src/components/SignupLogin/LoginDropdown.js
@@ -1,11 +1,14 @@
 import { ReactComponent as NoLogin } from '../../img/header/nologin.svg';
 
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 
 function LoginDropdown(props) {
   //傳入登入狀態，從App.js -> Header.js -> LoginDropdown.js
   const { isLogin, setIsLogin } = props;
+
+  const location = useLocation();
+  const history = useHistory();
 
   //dorpdown開關，讓.header_dropdown_menu會不會顯示出來？
   const [showDropdown, setShowDropdown] = useState('d-none');
@@ -87,6 +90,17 @@ function LoginDropdown(props) {
                 onClick={() => {
                   setShowDropdown('d-none');
                   setIsLogin(false);
+                  const paths = [
+                    '/pwdchanging',
+                    '/activity',
+                    '/fav',
+                    '/member',
+                    '/pwdchanging',
+                    '/order',
+                  ];
+                  if (paths.indexOf(location.pathname) > -1) {
+                    history.push('/Front');
+                  }
                 }}
               >
                 登出
