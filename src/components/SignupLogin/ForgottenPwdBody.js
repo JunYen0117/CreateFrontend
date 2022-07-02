3f33bfb2319ea04563d3c9978abcc11e29817a62
JunYen0117 <relife12345@gmail.com>
2022-07-02 21:53:38 +0800

    忘記密碼

--- src/components/SignupLogin/ForgottenPwdBody.js
+++ src/components/SignupLogin/ForgottenPwdBody.js
@@ -1,6 +1,18 @@
+import axios from 'axios';
+import { API_URL } from '../../utils/config';
+import { useState } from 'react';
 import LogoSvg1 from '../../img/header/logo.svg';
 
 function ForgottenPwdBody() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.get(API_URL + '/email');
+      console.log(response);
+    } catch (e) {
+      console.error('前端寄email錯誤' + e);
+    }
+  }
   return (
     <>
       <form className="forgotten_pwd_form">
@@ -13,7 +25,10 @@ function ForgottenPwdBody() {
             <input className="mx-auto mb-5" placeholder="請輸入註冊信箱" />
           </div>
           <div className="d-flex justify-content-center mb-4">
-            <button className="forgotten_pwd_btn mx-auto">
+            <button
+              className="forgotten_pwd_btn mx-auto"
+              onClick={handleSubmit}
+            >
               傳送密碼確認信至信箱
             </button>
           </div>
