1bfa2b17117ab2e36c43f45f01594639c89bd651
Miu37 <ispan8937@gmail.com>
2022-07-05 10:34:23 +0800

    Update CommentModal.js

--- src/components/ProductOrder/CommentModal.js
+++ src/components/ProductOrder/CommentModal.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import { API_URL } from '../../utils/config';
