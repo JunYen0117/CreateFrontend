aef508bb8a88519df702b545284ae957fb639ca5
immindy4527 <immindy4527@gmail.com>
2022-07-08 15:47:48 +0800

    關於我們完成!

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import CreateLogo from '../components/AboutUs/CreateLogo';
 import IntroduceUs from '../components/AboutUs/IntroduceUs';
+import Member from '../components/AboutUs/Member';
 
 function AboutUs() {
   return (
     <>
-      <div className="container">
+      <div className="container ">
         <CreateLogo />
         <IntroduceUs />
+        <Member />
       </div>
     </>
   );
